c126260a03817071774b75b05b3ee1d76f31d1ab
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-09-26 12:23:41 -0300

    updated sections and staff section

---
+++ src/config/Services.ts
@@ -0,0 +1,10 @@
+const services = [
+    {"title": "VR - 360", "img": ""},
+    {"title": "Pantalla Táctil", "img": ""},
+    {"title": "Holograma", "img": ""},
+    {"title": "IA", "img": ""},
+    {"title": "Realidad Aumentada", "img": ""},
+    {"title": "Servicio Transmedia", "img": ""}
+]
+
+export default services
---
+++ src/config/staff.ts
@@ -0,0 +1,7 @@
+const staff = [
+    {"title": "Nacho", "img": "/assets/foto.jpg"},
+    {"title": "Marco", "img": "/assets/foto.jpg"},
+    {"title": "Ale", "img": "/assets/foto.jpg"}
+]
+
+export default staff
