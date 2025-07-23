3e74ea505c3089b6a9cc5309cec56765d12bf9bf
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-07-23 18:50:25 -0300

    updated info

--- src/config/staff.ts
+++ src/config/staff.ts
@@ -1,7 +1,7 @@
 const staff = [
     {"title": "Pablo Puglisi", "img": "/assets/staff/pablo.webp", "desc": "CEO / Project Manager"},
-    {"title": "Marcos Dominguez", "img": "/assets/staff/marcos.webp", "desc": "CEO / CFO"},
-    {"title": "Sofia Fonteñez", "img": "/assets/staff/sofi.webp", "desc": "CEO / CCO"}
+    {"title": "Sofia Fonteñez", "img": "/assets/staff/sofi.webp", "desc": "CEO / CFO"},
+    {"title": "Marcos Dominguez", "img": "/assets/staff/marcos.webp", "desc": "CEO / CCO"}
 ]
 
 export default staff
